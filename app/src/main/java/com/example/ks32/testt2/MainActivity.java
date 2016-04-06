909f94806c8197211f9c42b5d54c26bdf1a32c2d
Asif Hisam <asif@pakdata.com>
2016-04-06 10:09:08 +0500

    Thanks to He Ke. He showed how to get it working with Crosswalk-Lite.

--- app/src/main/java/com/example/ks32/testt2/MainActivity.java
+++ app/src/main/java/com/example/ks32/testt2/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.ks32.testt2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.xwalk.core.XWalkActivity;
+import org.xwalk.core.XWalkView;
+import org.xwalk.core.internal.XWalkWebChromeClient;
 
+public class MainActivity extends XWalkActivity {
+    private XWalkView mXWalkView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onXWalkReady() {
         setContentView(R.layout.activity_main);
+        mXWalkView = (XWalkView) findViewById(R.id.mykView);
+        mXWalkView.load("http://crosswalk-project.org/", null);
     }
 }
